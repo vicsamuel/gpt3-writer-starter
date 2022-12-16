546560158ab884376c444f6f14a70e235a46954e
Vic <61720168+vicsamuel@users.noreply.github.com>
2022-12-16 04:04:17 +0000

    Connected to OpenAI API

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,32 @@ import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 import { useState } from 'react';
+import generateAction from './api/generate';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
   const onUserChangedText = (event) => {
     console.log(event.target.value);
     setUserInput(event.target.value);
@@ -13,15 +36,15 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Error Decoder</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Food Decider for my Wife</h1>
+            <h1>Error Decoder</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Error Decoder</h2>
+            <h2>Input your obscure coding error/code here</h2>
           </div>
         </div>
         <div className="prompt-container">
@@ -32,7 +55,7 @@ const Home = () => {
             onChange={onUserChangedText}
           />
           <div className="prompt-buttons">
-            <a className="generate-button" onClick={null}>
+            <a className="generate-button" onClick={callGenerateEndpoint}>
               <div className="generate">
                 <p>Generate</p>
               </div>
